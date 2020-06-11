e4e84e7aa8587059c1ecffc3b29f2a3b6e8cd811
k4i5er <kaiser@kaiser-hw01.localdomain>
2020-06-11 12:26:08 -0500

    Agregada función de búsqueda de registros por uno o dos términos

--- dict2.py
+++ dict2.py
@@ -23,10 +23,10 @@ agenda = [
             'first_name': 'Juan',
             'second_name': 'Arturo',
             'last_name': 'González',
-            'last_name2': 'Ramírez',
+            'last_name2': 'Gutiérrez',
         },
         'title': '',
-        'organization': '',
+        'organization': 'FacMat',
         'phone_number': {
             'cellphone': 7448372684,
             'work': 7447228192,
@@ -53,7 +53,7 @@ agenda = [
         'title': 'Profesor',
         'organization': 'Facultad de Matemáticas',
         'phone_number': {
-            'cellphone': 7440192543,
+            'cellphone': '',
             'work': 7449301548,
             'home': 7440192846,
         },
@@ -77,6 +77,57 @@ agenda = [
 # print(agenda[0]['notes'][1])
 
 # Mostrar primer nombre y primer apellido de todos los registros de la agenda
-for registro in agenda:
-    print(f'{registro["name"]["first_name"]} {registro["name"]["last_name"]}, {registro["title"]}\n{registro["organization"]}')
-    print('\n')
+# for registro in agenda:
+#     string = f'{registro["name"]["first_name"][0]}{registro["name"]["last_name"][0]} {registro["name"]["first_name"]} {registro["name"]["last_name"]}'
+#     if registro["title"] != "":
+#         string += ', ' + registro["title"]
+#     if registro["organization"] != "":
+#         string += '\n' + registro["organization"]
+#     print(string)
+#     print()
+
+# Salida de información
+# JP Juan Pérez
+# FacMat
+
+# CS Camilo Santiago Pérez, Profesor
+# Facultad de Matemáticas
+
+# Función de búsqueda que permita al usuario encontrar un registro en la agenda
+# por medio del nombre(s) y/o apellido(s)
+#
+
+# Juan Manuel Rodríguez
+
+# Escribe el término de búsqueda: Manuel Pérez
+
+
+def get_names(registro, st):
+    if registro["name"]["first_name"].lower() == st.lower() or registro["name"]["second_name"].lower() == st.lower() or registro["name"]["last_name"].lower() == st.lower() or registro["name"]["last_name2"].lower() == st.lower():
+        string = f'{registro["name"]["first_name"][0]}{registro["name"]["last_name"][0]} {registro["name"]["first_name"]} {registro["name"]["last_name"]} '
+        if registro["phone_number"]["cellphone"] != "":
+            string += f'Celular: {registro["phone_number"]["cellphone"]} '
+        if registro["phone_number"]["work"] != "":
+            string += f'Trabajo: {registro["phone_number"]["work"]} '
+        if registro["phone_number"]["home"] != "":
+            string += f'Casa: {registro["phone_number"]["home"]} '
+        return string
+    return False
+
+
+def search_contact(name):
+    if ' ' in name.lower():
+        st = name.split(' ')
+        for registro in agenda:
+            if get_names(registro, st[0]) != False:
+                print(get_names(registro, st[0]))
+            if get_names(registro, st[0]) == False and get_names(registro, st[1]) != False:
+                print(get_names(registro, st[1]))
+    else:
+        for registro in agenda:
+            print(get_names(registro, name))
+            print()
+    return
+
+
+search_contact("jaime GUtIérreZ")
